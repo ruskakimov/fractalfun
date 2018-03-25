2fbc0d02b9e2d6d8bead1fa7e11278fbc2cabe62
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-25 16:26:45 +0800

    did something fun

--- js/main.js
+++ js/main.js
@@ -8,4 +8,44 @@ canvas.width = 200;
 
 stretch(canvas);
 
-ctx.fillRect(50, 50, 100, 100);
+var lines = [
+    [0, 100, 200, 100]
+];
+
+drawLines();
+
+window.addEventListener('click', downTheRabbitHole);
+
+function rotate90(line) {
+    return [line[1], line[0], line[3], line[2]];
+}
+
+function halfs(line) {
+    var mid_x = Math.abs(line[0] - line[2]) / 2;
+    var mid_y = Math.abs(line[1] - line[3]) / 2;
+    return [
+        [line[0], line[1], mid_x, mid_y],
+        [mid_x, mid_y, line[2], line[3]]
+    ];
+}
+
+function downTheRabbitHole() {
+    var updated_lines = [];
+    lines.forEach(function(line) {
+        var rotated = rotate90(line);
+        updated_lines = updated_lines.concat(halfs(line));
+        updated_lines = updated_lines.concat(halfs(rotated));
+    });
+    lines = updated_lines;
+
+    drawLines();
+}
+
+function drawLines() {
+    ctx.beginPath;
+    lines.forEach(function(line) {
+        ctx.moveTo(line[0], line[1]);
+        ctx.lineTo(line[2], line[3]);
+    });
+    ctx.stroke();
+}
